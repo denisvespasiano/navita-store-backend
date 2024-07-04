069670878f219d7e8b667f14773c30d68ec19d00
quarkusio <no-reply@quarkus.io>
2024-07-04 16:53:13 +0000

    Initial commit

---
+++ src/test/java/com/navitanavita/store/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package com.navitanavita.store;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
